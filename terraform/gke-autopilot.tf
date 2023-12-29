e9ff429564bca385c5df411e3f7e09e0dd538de7
George <george.agriogiannis@suse.com>
2023-12-29 15:14:45 +0100

    Update gke-autopilot.tf

--- terraform/gke-autopilot.tf
+++ terraform/gke-autopilot.tf
@@ -3,16 +3,16 @@
 provider "google" {
   credentials = file("path/to/your/credentials.json")
   project     = "your-gcp-project-id"
-  region      = "europe-west1"  # Change this to your desired region in Europe
+  region      = "europe-west1"
 }
 
 provider "helm" {
-  version    >= "2.4.0"
+  version    = ">= 2.5.0"
   kubeconfig = module.gke_autopilot.kubeconfig
 }
 
 provider "kubernetes" {
-  version    >= "2.6.1"
+  version    = ">= 2.7.0"
   kubeconfig = module.gke_autopilot.kubeconfig
 }
 
@@ -31,7 +31,7 @@ module "network" {
   subnets = {
     my_subnet = {
       ip_cidr_range = "10.0.1.0/24"
-      region        = "europe-west1"  # Change this to your desired region in Europe
+      region        = "europe-west1"
     }
   }
 }
@@ -40,12 +40,10 @@ module "gke_autopilot" {
   source               = "github.com/terraform-google-modules/terraform-google-kubernetes-engine//modules/beta-autopilot-public-cluster?ref=main"
   project_id           = module.project.project_id
   cluster_name         = "myapp-gke-cluster"
-  location             = "europe-west1"  # Change this to your desired region in Europe
+  location             = "europe-west1" 
   network              = module.network.network_name
   subnetwork           = module.network.subnets.my_subnet.name
-  net_admin            = true  # Add the new option here if available
-
-  # Other configuration options...
+  net_admin            = true  # To allow for Istio setup
 
   outputs = {
     cluster_name     = module.gke_autopilot.cluster_name
@@ -57,8 +55,36 @@ module "gke_autopilot" {
 module "container_registry" {
   source               = "terraform-google-modules/container-registry/google"
   project_id           = module.project.project_id
-  location             = "europe-west1"  # Change this to your desired region in Europe
+  location             = "europe-west1"
   repository_name      = "myapp-container-repo"
+}
+
+resource "helm_release" "myapp" {
+  name      = "myapp"
+  namespace = "default"
+  chart     = "./helm"
+  values    = ["./helm-values/myapp.yaml"]
+}
+
+resource "helm_release" "istio-base" {
+  name      = "istio-base"
+  namespace = "istio-system"
+  chart     = "istio-base"
+  values    = ["./helm-values/istio-base.yaml"]
+}
+
+resource "helm_release" "istiod" {
+  name      = "istiod"
+  namespace = "istio-system"
+  chart     = "istiod"
+  values    = ["./helm-values/istiod.yaml"]
+}
+
+resource "helm_release" "istio-gateway" {
+  name      = "istio-gateway"
+  namespace = "istio-system"
+  chart     = "istio-ingressgateway"
+  values    = ["./helm-values/istio-gateway.yaml"]
 }
 
 output "gke_cluster_name" {
