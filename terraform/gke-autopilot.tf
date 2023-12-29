67b5c75a4a0cacf3b5df83a0047a0818a9f1e793
George <george.agriogiannis@suse.com>
2023-12-29 14:34:39 +0100

    Update gke-autopilot.tf

--- terraform/gke-autopilot.tf
+++ terraform/gke-autopilot.tf
@@ -3,7 +3,17 @@
 provider "google" {
   credentials = file("path/to/your/credentials.json")
   project     = "your-gcp-project-id"
-  region      = "us-central1"
+  region      = "europe-west1"  # Change this to your desired region in Europe
+}
+
+provider "helm" {
+  version    = "2.4.0"
+  kubeconfig = module.gke_autopilot.kubeconfig
+}
+
+provider "kubernetes" {
+  version    = "2.6.1"
+  kubeconfig = module.gke_autopilot.kubeconfig
 }
 
 module "project" {
@@ -16,31 +26,49 @@ module "project" {
 module "network" {
   source              = "terraform-google-modules/network/google"
   project_id          = module.project.project_id
-  network_name        = "my-vpc"
+  network_name        = "myapp-vpc"
   auto_create_subnetworks = false
   subnets = {
     my_subnet = {
       ip_cidr_range = "10.0.1.0/24"
-      region        = "us-central1"
+      region        = "europe-west1"  # Change this to your desired region in Europe
     }
   }
 }
 
 module "gke_autopilot" {
-  source               = "terraform-google-modules/kubernetes-engine/google//modules/autopilot"
+  source               = "github.com/terraform-google-modules/terraform-google-kubernetes-engine//modules/beta-autopilot-public-cluster?ref=main"
   project_id           = module.project.project_id
-  cluster_name         = "my-gke-cluster"
-  location             = "us-central1"
+  cluster_name         = "myapp-gke-cluster"
+  location             = "europe-west1"  # Change this to your desired region in Europe
   network              = module.network.network_name
   subnetwork           = module.network.subnets.my_subnet.name
   net_admin            = true  # Add the new option here if available
 
-  // Other configuration options...
+  # Other configuration options...
+
+  outputs = {
+    cluster_name     = module.gke_autopilot.cluster_name
+    cluster_endpoint = module.gke_autopilot.endpoint
+    kubeconfig       = module.gke_autopilot.kubeconfig
+  }
 }
 
 module "container_registry" {
   source               = "terraform-google-modules/container-registry/google"
   project_id           = module.project.project_id
-  location             = "us-central1"
-  repository_name      = "my-container-repo"
+  location             = "europe-west1"  # Change this to your desired region in Europe
+  repository_name      = "myapp-container-repo"
+}
+
+output "gke_cluster_name" {
+  value = module.gke_autopilot.cluster_name
+}
+
+output "gke_cluster_endpoint" {
+  value = module.gke_autopilot.endpoint
+}
+
+output "gke_kubeconfig" {
+  value = module.gke_autopilot.kubeconfig
 }
